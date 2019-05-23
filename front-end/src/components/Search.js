bde44a2a75130bd2277af08bffca120f60edcf3e
Matias Villalba <matias.gaston.villalba@gmail.com>
2019-05-23 07:53:01 -0300

    postman

--- front-end/src/components/Search.js
+++ front-end/src/components/Search.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import {searchPublicationsByTitle, getData} from "../actions"
-import uuidv1 from "uuid";
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import ClearIcon from '@material-ui/icons/Clear';
 import DirectionsIcon from '@material-ui/icons/Directions';
